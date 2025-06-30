999bd20ada3ca6287bf625896ff84b62a0183811
Abhinav Saraf <99379559+abhinav-saraf@users.noreply.github.com>
2025-06-30 08:43:02 +0530

    Create main.tf

---
+++ terraform/test/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "app_server" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  key_name = var.key_name
+  associate_public_ip_address = true
+  tags = {
+    Name = "FinanceMe-Test-Server"
+  }
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > ../ansible/inventory/test"
+  }
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file(var.private_key_path)
+    host  = self.public_ip
+  }
+}
