a0e0010472b60ea2e220f4a272a8a13437eeb051
Abhinav Saraf <99379559+abhinav-saraf@users.noreply.github.com>
2025-06-30 09:46:34 +0530

    Create variables.tf

---
+++ terraform/prod/variables.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "ami_id" {
+  description = "Ubuntu 22.04 AMI"
+  default     = "ami-0f5ee92e2d63afc18"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Your AWS EC2 Key Pair name"
+  default     = "your-key-name"
+}
+
+variable "private_key_path" {
+  description = "Path to your .pem key file"
+  default     = "~/.ssh/your-key.pem"
+}
