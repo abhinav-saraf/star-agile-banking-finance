7ea70a57e95c04690d75502820214da5d8358131
Abhinav Saraf <99379559+abhinav-saraf@users.noreply.github.com>
2025-07-01 12:37:08 +0530

    Update main.tf

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "app_server" {
   instance_type = var.instance_type
   key_name = var.key_name
   associate_public_ip_address =true
-  tags= {
+  tags = {
     Name = "FinanceMe-Prod-Server"
   }
 
