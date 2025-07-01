83dfc01de9c98eafc482eb9443b181baf6324bc6
Abhinav Saraf <99379559+abhinav-saraf@users.noreply.github.com>
2025-07-01 09:21:39 +0530

    Update variables.tf

--- terraform/test/variables.tf
+++ terraform/test/variables.tf
@@ -18,5 +18,5 @@ variable "key_name" {
 
 variable "private_key_path" {
   description = "Path to your .pem key file"
-  default     = "~/home/ubuntu/.ssh/id_rsa"
+  default     = "/home/ubuntu/.ssh/id_rsa"
 }
