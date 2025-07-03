d11150fa13a06be98cf3b290fb812ea943dc21c0
Abhinav Saraf <99379559+abhinav-saraf@users.noreply.github.com>
2025-07-03 12:55:35 +0530

    Update main.tf

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "app_server" {
   }
 
   provisioner "local-exec" {
-    command = "echo '[prod]\n${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa' > /var/lib/jenkins/workspace/FinanceMe/ansible/inventory/prod"
+    command = "echo ${self.public_ip} > /ansible/inventory/prod"
   }
 
   connection {
