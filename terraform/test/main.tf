25db9f47b97118a4c91b929d6d15820ac305eb7c
Abhinav Saraf <99379559+abhinav-saraf@users.noreply.github.com>
2025-07-03 11:47:21 +0530

    Update main.tf

--- terraform/test/main.tf
+++ terraform/test/main.tf
@@ -8,10 +8,7 @@ resource "aws_instance" "app_server" {
   }
 
   provisioner "local-exec" {
-    command = <<EOT
-  echo "[test]
-  ${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa" > /var/lib/jenkins/workspace/FinanceMe/ansible/inventory/test
-  EOT
+    command = "echo '[test]\n${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa' > /var/lib/jenkins/workspace/FinanceMe/ansible/inventory/test"
   }
 
   connection {
