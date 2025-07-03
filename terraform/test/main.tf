ddebb5754baebb5ce6e880c19ed366d763ce8512
Abhinav Saraf <99379559+abhinav-saraf@users.noreply.github.com>
2025-07-03 10:45:28 +0530

    Update main.tf

--- terraform/test/main.tf
+++ terraform/test/main.tf
@@ -3,20 +3,26 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami = "ami-020cba7c55df1f615"
-  instance_type = "t2.micro"
-  key_name = "id_rsa"
+  ami                         = "ami-020cba7c55df1f615"
+  instance_type               = "t2.micro"
+  key_name                    = "id_rsa"
   associate_public_ip_address = true
   tags = {
     Name = "FinanceMe-Test-Server"
   }
-  provisioner "local-exec" {
-    command = "echo '[test]\n${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa' > /var/lib/jenkins/workspace/FinanceMe/ansible/inventory/test"
-  }
+
   connection {
-    type = "ssh"
-    user = "ubuntu"
+    type        = "ssh"
+    user        = "ubuntu"
     private_key = file("/var/lib/jenkins/.ssh/id_rsa")
-    host  = self.public_ip
+    host        = self.public_ip
+  }
+}
+
+resource "null_resource" "generate_inventory" {
+  depends_on = [aws_instance.app_server]
+
+  provisioner "local-exec" {
+    command = "echo '[test]\n${aws_instance.app_server.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa' > /var/lib/jenkins/workspace/FinanceMe/ansible/inventory/test"
   }
 }
